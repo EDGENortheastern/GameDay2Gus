077919483d0ca3322f0d905167b970279611ff16
GustavErikson95 <54953754+GustavErikson95@users.noreply.github.com>
2022-03-02 16:55:39 +0000

    e

--- main.py
+++ main.py
@@ -1,64 +1,120 @@
 import math
+import random
 
-def baseOutput():
-  print("************")
-  for x in range(3):
-    print(" X | X | X ")
-  print("************")
-
-#Handles printing of board
-def introPrint(coorX, coorY,shotsFired):
-  # Prints Initial Border
-  for l in range(coorY):
-    print("****", end="")
-  print("*")
-  # Cycles Rows
-  for x in range(coorX):
-    # Cycles Columns
-    for y in range(coorY):
-      # Cycles shots fired
-      for z in shotsFired:
-        testingInt = char(x) + char(y)
-        print(testingInt)
-        
-
-        """""
-        if int(z%10) == y+1 and int(z-(z%10)/10) == x+1:
-          print("| 0 ",end="")
-        else:
-          # Prints Empty Rows
-          print("| X ",end="") 
-    print("|")
-        """
-  # Prints Ending Border
-  for x in range(coorY):
-    print("****", end="")
-  print("*")
-    
-# Receives initial Grid size
-gridSize = int(input("What is the grid size: "))
-gridY = int(gridSize%10)
-gridX = int(( gridSize - gridY ) / 10)
-# Receives Target Ship Location
-shipLocation = int(input("What is the ship location: "))
-shotsFired = []
-
-
-#Initialize Game
+# Sets initial flags
 gameOver = False
-while gameOver == False:
-  #Starts Fire Coordinate Check
-  fireLocation = int(input("What is the fire location: "))
-  # Checks if successful. Ends Game.
-  if fireLocation == shipLocation:
-    print("Hit! You sunk the Battleship!")
-    gameOver = True
-  # Appends shot to listing, prior removal prevents doubles.
-  try:
-    shotsFired.remove(fireLocation)
-  except ValueError:
-    shotsFired.append(fireLocation)
-  # Prints Current Board
-  introPrint(gridY,gridX,shotsFired)
+guessX = [-1]
+guessY = [-1]
+printThis = False
+enemyTotal = 1
+shipTarget = [0,0]
+
+#X is height, Y is width
+#Prints Grid
+def printPlainGrid(gX, gY):
+  # Set Global Flag
+  printThis = False
+  # Top Border
+  for y in range(gY):
+    print("****", end="")
+  print("*")
+  #Cycle Columns
+  for x in range(gX):
+    #Cycle Rows
+    for y in range(gY):
+      print("| ",end="")
+      #Cycle Guesses
+      for i in range(len(guessX)):
+        #Checks if any guesses equal the current cell being viewed. Changes Flag
+        if(guessX[i] == x and guessY[i] == y):
+          printThis = True
+      #Prints Fired Shot if Guess is Made. Else Prints X
+      if printThis == True:
+        print("0",end="")
+        printThis = False
+      else:
+        print("X",end="")
+      print(" ",end="")
+    print("|")
+  #Prints Lower Border
+  for x in range(gY):
+    print("****", end="")
+  print("*")
+
+#Appends X and Y to the list of Guesses only when [X,Y] is present within guessX,guessY
+def appendXY(aX, aY):
+  #print(guessX)
+  #print(guessY)
+  appCoor = True
+  for i in range(len(guessX)):
+    if(guessX[i] == aX and guessY[i] == aY):
+      appCoor = False
+  if (appCoor == True):
+    guessX.append(aX)
+    guessY.append(aY)
+  return appCoor
+    
+
+#Checks for hit in all guesses and returns true when true
+#OBSOLETE
+  """
+def checkHit():
+  if enemyTotal == 0:
+    return True
+  for i in range(len(guessX)):
+    for x in range(len(enemyX)):
+      for y in range(len(enemyY)):
+        if(guessX[i] == enemyX[x] and guessY[i] == enemyY[y]):
+          print("Hit! Ship sunk!")
+          enemyTotal -= 1
+          return False
+
+  return False
+  """
+    
+#Obtain size of grid.
+print("Lets play Battleship. First choose the size of your grid!")
+gridX = int(input("What is grid height: "))
+gridY = int(input("What is grid width: "))
+
+
+#Initialize enemy ships. 
+enemyTotal = 1
+enemyTotal = int(input("Please enter the total number of enemy ships: "))
+enemyX = []
+enemyY = []
+for i in range(enemyTotal):
+  enemyX.append(random.randint(0,gridX-1))
+  enemyY.append(random.randint(0,gridY-1))
+  #print(enemyX)
+  #print(enemyY)
   
+
+
+#Initialize Grid
+printPlainGrid(gridX,gridY)
+
+#starting loop
+while gameOver != True:
+  #Coor of next shot
+  appendCheck = True
+  shotX = int(input("Row of Shot: "))-1
+  shotY = int(input("Column of Shot: "))-1
+  appendCheck = appendXY(shotX,shotY)
+  if appendCheck == False:
+    continue
+  #print(guessX)
+  #print(guessY)
+  #Print Grid
+  printPlainGrid(gridX,gridY)
+  #Check for Victory Condition.
+  for i in range(len(guessX)):
+    for x in range(len(enemyX)):
+      for y in range(len(enemyY)):
+        if(guessX[i] == enemyX[x] and guessY[i] == enemyY[y]):
+          print("Hit! Ship sunk!")
+          enemyTotal -= 1
+          continue
+  if enemyTotal <= 0:
+    gameOver = True
   
